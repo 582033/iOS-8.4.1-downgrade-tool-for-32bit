350de29636d5a42f25af5400dd3387be066c9019
QuilTech <39478991+TheChosenOne650@users.noreply.github.com>
2019-01-24 11:58:38 -0700

    Update make_ipsw.sh

--- make_ipsw.sh
+++ make_ipsw.sh
@@ -12,22 +12,22 @@ select opt in "${options[@]}"
 do
     case $opt in
         "iPad 2 WiFi")
-         	echo building ipsw for iPad2,1 please wait...
+		echo building ipsw for iPad2,1 please wait...
 			./Tools/ipsw iPad2,1_8.4.1_12H321_Restore.ipsw iPad2,1_8.4.1_12H321_Odysseus.ipsw --memory --ota Tools/Manifests/K93AP.plist
 			echo ipsw creation complete
 			break
             ;;
         "iPad 2 GSM")
             echo building ipsw for iPad2,2 please wait...
-            ./Tools/ipsw iPad2,2_8.4.1_12H231_Restore.ipsw iPad2,2_8.4.1_12H321_Odysseus.ipsw --memory -bbupdate --ota Tools/Manifests/K94AP.plist
-            echo ipsw creation complete
-            break
+            		./Tools/ipsw iPad2,2_8.4.1_12H231_Restore.ipsw iPad2,2_8.4.1_12H321_Odysseus.ipsw --memory -bbupdate --ota Tools/Manifests/K94AP.plist
+            		echo ipsw creation complete
+            		break
             ;;
         "iPad 3 WiFi")
             echo building ipsw for iPad3,1 please wait...
-            ./Tools/ipsw iPad3,1_8.4.1_12H321_Restore.ipsw iPad3,1_8.4.1_12H321_Odysseus.ipsw --memory --ota Tools/Manifests/J1AP.plist
-            echo ipsw creation complete
-            break
+            		./Tools/ipsw iPad3,1_8.4.1_12H321_Restore.ipsw iPad3,1_8.4.1_12H321_Odysseus.ipsw --memory --ota Tools/Manifests/J1AP.plist
+            		echo ipsw creation complete
+            		break
             ;;
         "iPhone 4S")
             echo building ipsw for iPhone4,1 please wait...
@@ -36,16 +36,16 @@ do
 			break
             ;;
         "iPad Mini WiFi")
-			echo building ipsw for iPad2,5 please wait...
+		echo building ipsw for iPad2,5 please wait...
 			./Tools/ipsw iPad2,5_8.4.1_12H321_Restore.ipsw iPad2,5_8.4.1_12H321_Odysseus.ipsw --memory --ota Tools/Manifests/P105AP.plist
 			echo ipsw creation complete
 			break
             ;;
         "iPhone 5 GSM")
-            echo building ipsw for iPhone5,1 please wait...
-            ./Tools/ipsw iPhone5,1_8.4.1_12H321_Restore.ipsw iPhone5,1_8.4.1_12H321_Odysseus.ipsw --memory -bbupdate --ota Tools/Manifests/N41AP.plist
-            echo ipsw creation complete
-            break
+            	echo building ipsw for iPhone5,1 please wait...
+            		./Tools/ipsw iPhone5,1_8.4.1_12H321_Restore.ipsw iPhone5,1_8.4.1_12H321_Odysseus.ipsw --memory -bbupdate --ota Tools/Manifests/N41AP.plist
+            		echo ipsw creation complete
+           		break
             ;;
         "Quit")
             break
